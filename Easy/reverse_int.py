e1a43e84123b4d8494c292f7ad703e3612df07f9
Hareshku <hareesh7737@gmail.com>
2026-03-10 20:19:05 +0500

    Reverse integer

---
+++ Easy/reverse_int.py
@@ -0,0 +1,25 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        ans = 0
+        mi, mx = -(2**31), 2**31 - 1
+        
+        while x:
+            if ans < mi // 10 + 1 or ans > mx // 10:
+                return 0
+            
+            y = x % 10
+            if x < 0 and y > 0:
+                y -= 10
+            
+            ans = ans * 10 + y
+            x = (x - y) // 10
+        
+        return ans
+
+
+# main program
+s = Solution()
+
+print(s.reverse(123))
+print(s.reverse(-123))
+print(s.reverse(120))
