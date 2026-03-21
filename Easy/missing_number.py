6f3eab6954f195e74b2953d9a458f8c4d415a326
Hareshku <hareesh7737@gmail.com>
2026-03-21 15:35:29 +0500

    Find missing number

--- Easy/missing_number.py
+++ Easy/missing_number.py
@@ -1,22 +1,24 @@
 from functools import reduce
 from operator import xor
 from typing import List
-class Solution:
-  def findNum(self, nums: list[int]) -> int:
-    n = len(nums)
-    for i in range(n):
-      if nums[i] != i:
-        return i
-list1 = [0, 1, 2, 3, 4, 5, 6, 8, 9]
-obj=Solution()
-print(obj.findNum(list1))
+
+#  for sorted list
+# class Solution:
+#   def findNum(self, nums: list[int]) -> int:
+#     n = len(nums)
+#     for i in range(n):
+#       if nums[i] != i:
+#         return i
+# list1 = [0, 1, 2, 3, 4, 5, 6, 8, 9]
+# obj=Solution()
+# print(obj.findNum(list1))
 
 
 # optimal solution
-# class Solution:
-#     def missingNumber(self, nums: List[int]) -> int:
-#         return reduce(xor, (i ^ v for i, v in enumerate(nums, 1)))
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        return reduce(xor, (i ^ v for i, v in enumerate(nums, 1)))
       
-# list1 = [0, 1, 2, 3, 4, 5, 6, 8, 9]
-# obj=Solution()
-# print(obj.missingNumber(list1))
+list1 = [3, 0, 1]
+obj=Solution()
+print(obj.missingNumber(list1))
