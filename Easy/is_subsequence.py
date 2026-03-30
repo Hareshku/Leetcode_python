5a2a76da8c525ab68b7b1787f091cb5103e6bcf6
Hareshku <hareesh7737@gmail.com>
2026-03-30 16:07:35 +0500

    Is subsequence

---
+++ Easy/is_subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j, m, n = 0, 0, len(s), len(t)
+        while i < m and j < n:
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return i == m
+
+s = "abc"
+t = "ahbgdc"
+
+obj = Solution()
+print(obj.isSubsequence(s, t))
