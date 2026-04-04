05f396fd96c1321fc5fed6e177d2d615dfc15bcf
Hareshku <hareesh7737@gmail.com>
2026-04-04 19:23:31 +0500

    Convert given string to lower case

---
+++ Easy/to_lower_case.py
@@ -0,0 +1,7 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return "".join([chr(ord(c) | 32) if c.isupper() else c for c in s])
+      
+s = "HELLO"
+obj = Solution()
+print(obj.toLowerCase(s))
