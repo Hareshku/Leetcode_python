e697d13d0f2a9f1fcc69d9e27c4c3ada3abfcef0
Hareshku <hareesh7737@gmail.com>
2026-04-05 16:32:34 +0500

    Max product of three numbers

---
+++ Easy/max_product_of_3_no.py
@@ -0,0 +1,12 @@
+from heapq import nlargest
+from typing import List
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        top3 = nlargest(3, nums)
+        bottom2 = nlargest(2, nums, key=lambda x: -x)
+        return max(top3[0] * top3[1] * top3[2], top3[0] * bottom2[0] * bottom2[1])
+      
+      
+nums = [1,2,3,4]
+obj = Solution()
+print(obj.maximumProduct(nums))
