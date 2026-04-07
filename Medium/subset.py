212a4049ad6de2906a7221fa9cec55c2eaf07d25
Hareshku <hareesh7737@gmail.com>
2026-04-07 13:05:13 +0500

    Find subsets of a given set

---
+++ Medium/subset.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution: # bfs
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return [[]]
+
+        nums.sort() # sort() not necessary if no duplicates
+        result = [[]]
+
+        for num in nums:
+            result += [subset + [num] for subset in result]
+
+        return result
+      
+nums = [1,2,3]
+
+obj = Solution()
+print(obj.subsets(nums))
