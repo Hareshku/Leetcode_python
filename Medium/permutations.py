8a5b6e707b436a27f790b00b9e5e2f540554ade7
Hareshku <hareesh7737@gmail.com>
2026-04-09 20:53:31 +0500

    Permutations

---
+++ Medium/permutations.py
@@ -0,0 +1,24 @@
+from typing import List
+class Solution: # iterative
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+
+        if nums is None or len(nums) == 0:
+            return ans
+
+        for num in nums:
+            new_res = []
+            for perm in res:
+                for i in range(len(perm) + 1):
+                    new_perm = perm[:i] + [num] + perm[i:]
+                    new_res.append(new_perm)
+
+            res = new_res
+
+        return res
+
+
+nums = [1,2,3]
+
+obj = Solution()
+print(obj.permute(nums))
