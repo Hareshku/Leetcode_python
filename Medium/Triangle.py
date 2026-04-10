acef82fe11f93d19abc65dd0b65ec64fde38940c
Hareshku <hareesh7737@gmail.com>
2026-04-10 14:18:10 +0500

    Triangle

---
+++ Medium/Triangle.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1):
+                triangle[i][j] = (
+                    min(triangle[i + 1][j], triangle[i + 1][j + 1]) + triangle[i][j]
+                )
+        return triangle[0][0]
+
+
+triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+
+obj = Solution()
+print(obj.minimumTotal(triangle))
