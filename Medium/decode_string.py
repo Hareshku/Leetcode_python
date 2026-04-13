24b5dcf12b350f2ad2ea0473d46deec91a088a52
Hareshku <hareesh7737@gmail.com>
2026-04-13 17:06:14 +0500

    Decode string

---
+++ Medium/decode_string.py
@@ -0,0 +1,21 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        num_stk, str_stk = [], []
+        num, res = 0, ''
+        for c in s:
+            if c.isdigit():
+                num = num * 10 + int(c)
+            elif c == '[':
+                num_stk.append(num)
+                str_stk.append(res) # initial, res being pushed is empty str ''
+                num, res = 0, ''
+            elif c == ']':
+                res = str_stk.pop() + res * num_stk.pop()
+            else:
+                res += c
+        return res
+
+s = "3[a]2[bc]"
+
+obj = Solution()
+print(obj.decodeString(s))
