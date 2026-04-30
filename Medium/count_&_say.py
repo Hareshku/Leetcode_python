a1ee60b4506395eda44f5e1d019c8fb9a9d9b9ab
Hareshku <hareesh7737@gmail.com>
2026-04-30 16:41:03 +0500

    Count and say

---
+++ Medium/count_&_say.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        s = '1'
+        for _ in range(n - 1):
+            i = 0
+            t = []
+            while i < len(s):
+                j = i
+                while j < len(s) and s[j] == s[i]:
+                    j += 1
+                t.append(str(j - i))
+                t.append(str(s[i]))
+                i = j
+            s = ''.join(t)
+        return s
+
+
+# Driver code
+obj = Solution()
+result = obj.countAndSay(4)   # change 5 to any value
+print(result)
