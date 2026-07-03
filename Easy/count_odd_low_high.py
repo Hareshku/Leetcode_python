fde98089e01538779fda3e7c56903fc9a417e7fb
Hareshku <hareesh7737@gmail.com>
2026-07-03 13:10:26 +0500

    Find odd and even numbers between two given numbers

--- Easy/count_odd_low_high.py
+++ Easy/count_odd_low_high.py
@@ -1,3 +1,29 @@
-class Solution:
-    def countOdds(self, low: int, high: int) -> int:
-        return ((high+1)//2 - (low//2))
+# for finding odd numbers between 1 to n we have formula (n+1)/2
+#  for finding the even number we do (high)//2 - (low-1)//2
+
+# Odd numbers 
+
+
+low = int(input("Enter low number: "))
+high = int(input("Enter high number: "))
+count = ((high+1)//2 - (low)//2)
+list1= []
+for i in range (low, high+1):
+  if i%2 !=0:
+    list1.append(i)
+
+print(count, list1)
+
+
+
+# Even Numbers 
+
+low = int(input("Enter low number: "))
+high = int(input("Enter high number: "))
+count = ((high)//2 - (low-1)//2)
+list1= []
+for i in range (low, high+1):
+  if i%2 ==0:
+    list1.append(i)
+
+print(count, list1)
