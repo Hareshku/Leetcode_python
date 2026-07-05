a2055962e7e9a64742a85eff9aeb63c3ba63c72f
Hareshku <hareesh7737@gmail.com>
2026-07-05 21:36:24 +0500

    updated

--- Easy/Subtract_Product_Sum_Digits_Int.py
+++ Easy/Subtract_Product_Sum_Digits_Int.py
@@ -1,13 +1,13 @@
 n=234
 temp = n
-sum =0
+sum_ =0
 product= 1
 while temp>0:
   r= temp%10
-  sum+=r
+  sum_+=r
   product*=r
   temp//=10
 
-result= product-sum
+result= product-sum_
 
 print(result)
