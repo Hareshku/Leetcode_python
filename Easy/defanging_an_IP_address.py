013a9563450089ef9003288d91b631bacff7d60c
Hareshku <hareesh7737@gmail.com>
2026-07-24 22:54:31 +0500

    Check if a given string is palindrom without any builtin method

--- Easy/defanging_an_IP_address.py
+++ Easy/defanging_an_IP_address.py
@@ -4,14 +4,49 @@
 #         return address.replace(".", "[.]")
 
 
-# without replace function using loop
-address = "1.1.1.1"
-ans = " "
-for i in address:
-    if i != ".":
-        ans += i
+# # without replace function using loop
+# address = "1.1.1.1"
+# ans = " "
+# for i in address:
+#     if i != ".":
+#         ans += i
 
+#     else:
+#         ans += "[.]"
+
+# print(ans)
+
+
+val_pal = "1223"
+# reverse = ''.join(reversed(val_pal))
+# if val_pal == reverse:
+#     print("p")
+# else:
+#     print("n")
+
+# print(val_pal)
+# print(reverse)
+
+
+def alphanumeric(s):
+    x = ord(s)
+    if 65 <= x <= 90 or 97 <= x <= 122 or 48 <= x <= 57:
+        return True
     else:
-        ans += "[.]"
+        return False
 
-print(ans)
+
+s = val_pal.lower()
+i = 0
+j = len(val_pal)
+
+while i < j:
+    if not alphanumeric(s[i]):
+        i += 1
+    elif not alphanumeric(s[j]):
+        j -= 1
+    elif s[i] == s[j]:
+        i += 1
+        j -= 1
+    else:
+        print("false")
--- Easy/valid_palindrom.py
+++ Easy/valid_palindrom.py
@@ -1,3 +1,4 @@
+# First method to check if an string is palindrom using builtin isalnum() method
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         i, j = 0, len(s) - 1
@@ -11,6 +12,34 @@ class Solution:
             else:
                 i, j = i + 1, j - 1
         return True
-s="A man, a plan, a canal: Panama"
-obj= Solution()
+
+
+s = "A man, a plan, a canal: Panama"
+obj = Solution()
 print(obj.isPalindrome(s))
+
+
+# Check if an string is palindrom 2nd method
+class Solution:
+    def isAlphanumeric(self, s):
+        x = ord(s)
+        if 65 <= x <= 90 or 97 <= x <= 122 or 48 <= x <= 57:
+            return True
+        return False
+
+    def isPalindrome(self, s: str) -> bool:
+        str = s.lower()
+        i = 0
+        j = len(str)-1
+
+        while i < j:
+            if not self.isAlphanumeric(str[i]):
+                i += 1
+            elif not self.isAlphanumeric(str[j]):
+                j -= 1
+            elif str[i] == str[j]:
+                i += 1
+                j -= 1
+            else:
+                return False
+        return True
