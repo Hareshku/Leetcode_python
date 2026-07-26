98780d41bad948eb77fd47910dbf70ca7527d5f7
Hareshku <hareesh7737@gmail.com>
2026-07-26 23:44:56 +0500

    Alternative way of reversing a string

--- Easy/reverse_string.py
+++ Easy/reverse_string.py
@@ -1,13 +1,22 @@
 from typing import List
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         """
+#         Do not return anything, modify s in-place instead.
+#         """
+#         s[:] = s[::-1]
+
+
+# str= ["h", "e", "l", "l", "o"]
+# print(str)
+# obj= Solution()
+# obj.reverseString(str)
+# print(str)
+
+# Second way
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
-        s[:] = s[::-1]
-
-str= ["h", "e", "l", "l", "o"]
-print(str)
-obj= Solution()
-obj.reverseString(str)
-print(str)
+        s.reverse()
