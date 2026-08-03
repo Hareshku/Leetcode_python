d058e1078355ac572bdf8c782584d853484e9d4b
Hareshku <hareesh7737@gmail.com>
2026-08-03 21:39:06 +0500

    Reverse words in a string

--- Medium/revers_words.py
+++ Medium/revers_words.py
@@ -2,8 +2,18 @@ class Solution:
     def reverseWords(self, s: str) -> str:
         words = s.strip().split()
         return ' '.join(words[::-1])
-      
-s =  "the sky is blue"
+
+
+s = "the sky is blue"
 
 obj = Solution()
 print(obj.reverseWords(s))
+
+# Way 2
+
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = (s.strip()).split()
+        s.reverse()
+        return " ".join(s)
