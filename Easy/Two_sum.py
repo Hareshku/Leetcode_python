dddbef08cce7387e69748cb36502358af258be4f
Hareshku <hareesh7737@gmail.com>
2026-08-04 23:56:03 +0500

    Two sum problem

--- Easy/Two_sum.py
+++ Easy/Two_sum.py
@@ -7,7 +7,7 @@
 #             if y in m:
 #                 return [m[y], i]
 #             m[x] = i
-            
+
 # # test the function
 # s = Solution()
 # # nums = [2,7,11,15]
@@ -18,33 +18,48 @@
 # # target = 6
 
 
-
 # result = s.twoSum(nums, target)
 
 # print(result)
 
 
 from typing import List
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         m = {}
         for i, x in enumerate(nums):
-            y = target - x # y=7
-            if y in m: # false
-                return [m[y], i] # [0,1]
-            m[x] = i # m={2:0}
-            
+            y = target - x  # y=7
+            if y in m:  # false
+                return [m[y], i]  # [0,1]
+            m[x] = i  # m={2:0}
+
+
 # test the function
 s = Solution()
 # nums = [2,7,11,15]
 # target = 9
-nums = [3,2,4]
+nums = [3, 2, 4]
 target = 6
 # nums = [3,3]
 # target = 6
 
 
-
 result = s.twoSum(nums, target)
 
 print(result)
+
+# second way
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        dict1 = {}
+
+        for i in range(n):
+            rem = target - nums[i]
+            if rem in dict1:
+                return [dict1[rem], i]
+            dict1[nums[i]] = i
