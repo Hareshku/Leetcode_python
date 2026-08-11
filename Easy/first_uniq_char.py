0b05f08044479009ccc8ce3d70b19e97e273c10c
Hareshku <hareesh7737@gmail.com>
2026-08-11 23:20:17 +0500

    Find first unique character in the string

--- Easy/first_uniq_char.py
+++ Easy/first_uniq_char.py
@@ -1,4 +1,8 @@
+# First way to do this problem
+
 from typing import Counter
+
+
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         cnt = Counter(s)
@@ -7,6 +11,25 @@ class Solution:
                 return i
         return -1
 
+
 s = "leetcode"
 obj = Solution()
 print(obj.firstUniqChar(s))
+
+# Second way
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        freq = {}
+
+        for i in s:
+            if i not in freq:
+                freq[i] = 1
+            else:
+                freq[i] += 1
+
+        for i in range(len(s)):
+            if freq[s[i]] == 1:
+                return i
+        return -1
