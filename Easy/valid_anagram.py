1e79202aa0074dc7cd7905af9c7cbf184ca6e948
Hareshku <hareesh7737@gmail.com>
2026-08-12 23:58:10 +0500

    valid anagram

--- Easy/valid_anagram.py
+++ Easy/valid_anagram.py
@@ -1,4 +1,6 @@
 from typing import Counter
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
@@ -9,9 +11,37 @@ class Solution:
             if cnt[c] < 0:
                 return False
         return True
-      
+
+
 s = "anagram"
 t = "nagaram"
 
 obj = Solution()
 print(obj.isAnagram(s, t))
+
+
+# 2nd way
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        freq = {}
+
+        for i in s:
+            if i not in freq:
+                freq[i] = 1
+            else:
+                freq[i] += 1
+
+        for i in t:
+            if i not in freq:
+                False
+            else:
+                freq[i] -= 1
+
+        for i in freq.values():
+            if i != 0:
+                False
+        return True
