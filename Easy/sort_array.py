e9dba37b9a076d29f6b07e2c3a6d45f025b9cbf8
Hareshku <hareesh7737@gmail.com>
2026-08-18 23:28:12 +0500

    Sort an array without builtin method

---
+++ Easy/sort_array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        # first way 
+        # nums.sort()
+        # return nums
+
+        # 2nd way 
+        for i in range(n):
+            for j in range(n-i-1):
+                if nums[j]>nums[j+1]:
+                    temp = nums[i]
+                    nums[i]= nums[j+1]
+                    nums[j+1]=temp
+        return nums
+
+
+
