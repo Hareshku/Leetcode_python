66159169e470fd84de41943257b14403144325b9
Hareshku <hareesh7737@gmail.com>
2026-08-19 23:14:25 +0500

    Sort an array without built in method

--- Easy/sort_array.py
+++ Easy/sort_array.py
@@ -6,11 +6,15 @@ class Solution:
 
         # 2nd way 
         for i in range(n):
+            isSwap = False
             for j in range(n-i-1):
                 if nums[j]>nums[j+1]:
                     temp = nums[i]
                     nums[i]= nums[j+1]
                     nums[j+1]=temp
+                    isSwap = True
+            if not isSwap:
+                break
         return nums
 
 
