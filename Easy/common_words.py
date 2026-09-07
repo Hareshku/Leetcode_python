55acd40ed95a0431908b1b8579ad88ece2b6d463
Hareshku <hareesh7737@gmail.com>
2026-09-07 13:08:43 +0500

    Basic leetcode problems solved

---
+++ Easy/common_words.py
@@ -0,0 +1,123 @@
+# # Lecture-1
+
+# # FizzBuzz
+# class Solution:
+#     def fizzBuzz(self, n: int) -> List[str]:
+#         ans = []
+
+#         for i in range(1, n+1):
+#             if i%3 == 0 and i%5==0:
+#                 ans.append("FizzBuzz")
+#             elif i%3==0:
+#                 ans.append("Fizz")
+#             elif i%5 == 0:
+#                 ans.append("Buzz")
+#             else:
+#                 ans.append(str(i))
+#         return ans
+
+
+# # Count Odd numbers in an interval range
+# class Solution:
+#     def countOdds(self, low: int, high: int) -> int:
+#         # formula to find odd numbers from 1 to n : (n+1)/2. From 1-low= low-1
+
+#         return (high+1)//2- (low//2)
+
+# # Other way
+
+# ans = 0
+# for i in range(low, high+1):
+#     if i%2 !=0:
+#         ans+=1
+# print(ans)
+
+
+# # Kids with greatest numbers of Candies
+
+# class Solution:
+#     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+#         new_list =[]
+#         maxCandies = max(candies)
+#         for i in candies:
+#             if (i+extraCandies)>=maxCandies:
+#                 new_list.append(True)
+#             else:
+#                 new_list.append(False)
+
+#         return new_list
+
+
+
+# # Palindrome number
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         temp = x
+#         reverse= 0
+#         while temp >0:
+#             r = temp%10
+#             reverse=(reverse*10+r)
+#             temp//=10
+#         # if x == reverse:
+#         #     return True
+#         # else:
+#         #     return False
+#         return reverse==x
+        
+# # Substract the product and sum of digits of an integer
+# class Solution:
+#     def subtractProductAndSum(self, n: int) -> int:
+#         sum =0
+#         product =1
+#         while n>0:
+#             r = n%10
+#             n //=10
+#             sum +=r
+#             product*=r
+#         return product - sum
+        
+
+# # Count the digits that divides a number
+# class Solution:
+#     def countDigits(self, num: int) -> int:
+#         ans = 0
+#         temp =num
+
+#         while temp>0:
+#             r = temp%10
+#             if num %r == 0:
+#                 ans+=1
+#             temp//=10
+#         return ans
+
+
+# # How many numbers are the small than the current number 
+# class Solution:
+#     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+#         ans =[]
+#         for i in nums:
+#             count = 0
+#             for j in nums:
+#                 if i>j:
+#                     count+=1
+#             ans.append(count)
+#         return ans
+        
+
+# Sort an array 
+# first way using buildtin method
+numbers = [5, 3, 8, 1, 2]
+numbers.sort() # Sorts in ascending order
+print(numbers) 
+
+# 2nd way without builtin method 
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(n-i-1):
+                if nums[j]>nums[j+1]:
+                    temp = nums[j]
+                    nums[j]= nums[j+1]
+                    nums[j+1]=temp
+        return nums
