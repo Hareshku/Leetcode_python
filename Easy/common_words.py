d1b66889ea1d2b0253f9b26ed84a12065db66a2f
Hareshku <hareesh7737@gmail.com>
2026-09-08 11:50:45 +0500

    Basic leetcode problems solved

--- Easy/common_words.py
+++ Easy/common_words.py
@@ -1,6 +1,7 @@
 # # Lecture-1
 
 # # FizzBuzz
+# Time complexity O(n)
 # class Solution:
 #     def fizzBuzz(self, n: int) -> List[str]:
 #         ans = []
@@ -18,6 +19,7 @@
 
 
 # # Count Odd numbers in an interval range
+# Time complexiy O(1)
 # class Solution:
 #     def countOdds(self, low: int, high: int) -> int:
 #         # formula to find odd numbers from 1 to n : (n+1)/2. From 1-low= low-1
@@ -25,7 +27,7 @@
 #         return (high+1)//2- (low//2)
 
 # # Other way
-
+#  Time complexity O(n)
 # ans = 0
 # for i in range(low, high+1):
 #     if i%2 !=0:
@@ -34,7 +36,7 @@
 
 
 # # Kids with greatest numbers of Candies
-
+# Big O(n)
 # class Solution:
 #     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
 #         new_list =[]
@@ -50,6 +52,7 @@
 
 
 # # Palindrome number
+# O(log n)
 # class Solution:
 #     def isPalindrome(self, x: int) -> bool:
 #         temp = x
@@ -65,6 +68,7 @@
 #         return reverse==x
         
 # # Substract the product and sum of digits of an integer
+#  O(log n)
 # class Solution:
 #     def subtractProductAndSum(self, n: int) -> int:
 #         sum =0
@@ -78,6 +82,7 @@
         
 
 # # Count the digits that divides a number
+# O(log n)
 # class Solution:
 #     def countDigits(self, num: int) -> int:
 #         ans = 0
@@ -92,6 +97,7 @@
 
 
 # # How many numbers are the small than the current number 
+# O(n**2)
 # class Solution:
 #     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
 #         ans =[]
@@ -106,11 +112,13 @@
 
 # Sort an array 
 # first way using buildtin method
+O(n log n)
 numbers = [5, 3, 8, 1, 2]
 numbers.sort() # Sorts in ascending order
 print(numbers) 
 
 # 2nd way without builtin method 
+O(n**2)
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         n = len(nums)
